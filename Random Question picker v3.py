41a6a925924e624d4210ed420fd08a44d9f9ef9c
santasack <jackuknz@gmail.com>
2023-05-15 11:37:42 +1200

    .

---
+++ Random Question picker v3.py
@@ -0,0 +1,35 @@
+# Random question picker v3
+# Same as v2 but it actually selects the question and displays it to the user
+
+import random
+
+
+questions = {
+        "What is the English word for 'Kohitātea'?": "January",
+        "What is the English word for 'Hui-tanguru'?": "February",
+        "What is the English word for 'Poutū-te-rangi'?": "March",
+        "What is the English word for 'Paenga-whāwhā'?": "April",
+        "What is the English word for 'Haratua'?": "May",
+        "What is the English word for 'Pipiri'?": "June",
+        "What is the English word for 'Hōngongoi'?": "July",
+        "What is the English word for 'Here-turi-kōkā'?": "August",
+        "What is the English word for 'Mahuru'?": "September",
+        "What is the English word for 'Whiringa-ā-nuku'?": "October",
+        "What is the English word for 'Whiringa-ā-rangi'?": "November",
+        "What is the English word for 'Hakihea'?": "December",
+        "What is the English word for 'tahi'?": "one",
+        "What is the English word for 'rua'?": "two",
+        "What is the English word for 'toru'?": "three",
+        "What is the English word for 'whā'?": "four",
+        "What is the English word for 'rima'?": "five",
+        "What is the English word for 'ono": "six",
+        "What is the English word for 'whitu'?": "seven",
+        "What is the English word for 'waru'?": "eight",
+        "What is the English word for 'iwa'?": "nine",
+        "What is the English word for 'tekau'?": "ten"
+    }
+
+questions_list = list(questions.items())
+random.shuffle(questions_list)
+for question, answer in questions_list[:10]:
+        user_answer = input(question + " ")
---
+++ Random Question picker.py
@@ -0,0 +1,6 @@
+# Random question picker v1
+
+import random
+
+questions_list = list(questions.items())
+random.shuffle(questions_list)
